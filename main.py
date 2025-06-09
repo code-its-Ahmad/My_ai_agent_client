7953f0824cc5d21097be57276c7cba2586432bdf
Muhammad Ahmad <166840462+code-its-Ahmad@users.noreply.github.com>
2025-06-09 16:34:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -286,4 +286,4 @@ async def submit_contact_request(details: ContactDetails):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
